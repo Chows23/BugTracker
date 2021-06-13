6819272639c76ecec89a89907a08ed7032869c83
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2021-06-13 16:43:19 -0500

    add basic ticketpriorityservice

---
+++ Bug_Tracker/BL/TicketPriorityService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bug_Tracker.DAL;
+using Bug_Tracker.Models;
+
+namespace Bug_Tracker.BL
+{
+    public class TicketPriorityService
+    {
+        public TicketPriorityRepo repo;
+        public void Create(TicketPriority ticketPriority)
+        {
+            repo.Add(ticketPriority);
+        }
+    }
+}

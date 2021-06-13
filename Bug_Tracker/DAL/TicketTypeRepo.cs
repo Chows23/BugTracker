9e3a031243b20582617992bbd7c68684677674e1
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2021-06-13 16:48:14 -0500

    add TicketTypeRepo

---
+++ Bug_Tracker/DAL/TicketTypeRepo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bug_Tracker.Models;
+
+namespace Bug_Tracker.DAL
+{
+    public class TicketTypeRepo : IRepository<TicketType>
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public void Add(TicketType entity)
+        {
+            db.TicketTypes.Add(entity);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<TicketType> GetCollection(Func<TicketType, bool> condition)
+        {
+            return db.TicketTypes.Where(condition);
+        }
+
+        public TicketType GetEntity(int id)
+        {
+            return db.TicketTypes.Find(id);
+        }
+
+        public TicketType GetEntity(Func<TicketType, bool> condition)
+        {
+            return db.TicketTypes.FirstOrDefault(condition);
+        }
+
+        public void Update(TicketType entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

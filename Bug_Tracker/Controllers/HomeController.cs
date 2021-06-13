17615d1a783aa0122c50d12b4110d635ad84163d
Katherine Desrosiers <katbdesrosiers@gmail.com>
2021-06-13 18:43:31 -0500

    remove contact page

--- Bug_Tracker/Controllers/HomeController.cs
+++ Bug_Tracker/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace Bug_Tracker.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }

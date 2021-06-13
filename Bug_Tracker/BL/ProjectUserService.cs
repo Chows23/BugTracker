25c80766dc4ca7a7460ee64026ffc7f1a9a58122
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2021-06-13 16:28:55 -0500

    add basic ProjectUserService

---
+++ Bug_Tracker/BL/ProjectUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bug_Tracker.Models;
+using Bug_Tracker.DAL;
+
+namespace Bug_Tracker.BL
+{
+    public class ProjectUserService
+    {
+        public ProjectUserRepo repo;
+        public void Create(ProjectUser projectUser)
+        {
+            repo.Add(projectUser);
+        }
+    }
+}

32319fdc77d2016bc9ef559e773d21b4dc1d494a
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2021-06-13 18:04:34 -0500

    add basic Index action in project controller

--- Bug_Tracker/BL/UserService.cs
+++ Bug_Tracker/BL/UserService.cs
@@ -1,10 +1,10 @@
 ﻿using Bug_Tracker.Models;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity;
 
 namespace Bug_Tracker.BL
 {
--- Bug_Tracker/Controllers/ProjectsController.cs
+++ Bug_Tracker/Controllers/ProjectsController.cs
@@ -4,16 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Bug_Tracker.BL;
+using Bug_Tracker.Models;
+using Microsoft.AspNet.Identity;
 
 namespace Bug_Tracker.Controllers
 {
+    [Authorize]
     public class ProjectsController : Controller
     {
-        public ProjectService projectService { get; set; }
+        private ProjectService projectService = new ProjectService();
+        private UserService userService = new UserService();
         public ActionResult Index()
         {
-
-            return View();
+            ApplicationUser user;
+            if (User.Identity.IsAuthenticated)
+                user = (ApplicationUser)User.Identity;
+            else
+                return new HttpUnauthorizedResult();
+            
+            return View(user.ProjectUsers.Select(p => p.Project));
         }
     }
 }

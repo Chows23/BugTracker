d03ca7829ccedf1a11905ea5c8f5868ebf1c5c92
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2021-06-13 14:44:27 -0500

    create IRepository interface

---
+++ Bug_Tracker/DAL/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bug_Tracker.DAL
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetEntity(int id);
+        T GetEntity(Func<T, bool> condition);
+        IEnumerable<T> GetCollection(Func<T, bool> condition);
+    }
+}
---
+++ Bug_Tracker/DAL/ProjectRepo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bug_Tracker.Models;
+
+namespace Bug_Tracker.DAL
+{
+    public class ProjectRepo : IRepository<Project>
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public void Add(Project entity)
+        {
+            
+        }
+
+        public void Delete(Project entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Project> GetCollection(Func<Project, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project GetEntity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project GetEntity(Func<Project, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Project entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

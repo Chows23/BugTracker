d13c995c63a84330684d6300a266a987370ca10a
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2021-06-13 17:07:13 -0500

    add empty project controller and index view

---
+++ Bug_Tracker/Controllers/ProjectsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bug_Tracker.BL;
+
+namespace Bug_Tracker.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public ProjectService projectService { get; set; }
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}

343e8fff419cfb390ce22cdfd2da9ea6f80992eb
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2021-06-13 16:20:22 -0500

    add ProjectUserRepo

---
+++ Bug_Tracker/DAL/ProjectUserRepo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bug_Tracker.Models;
+
+namespace Bug_Tracker.DAL
+{
+    public class ProjectUserRepo : IRepository<ProjectUser>
+    {
+        public ApplicationDbContext db = new ApplicationDbContext();
+        public void Add(ProjectUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProjectUser> GetCollection(Func<ProjectUser, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProjectUser GetEntity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProjectUser GetEntity(Func<ProjectUser, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ProjectUser entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

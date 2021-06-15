054f1261c8b930343bf9d245cc3d14db07a6fea9
Katherine Desrosiers <katbdesrosiers@gmail.com>
2021-06-14 19:30:30 -0500

    update dashboard functions for recent projects

--- Bug_Tracker/Controllers/HomeController.cs
+++ Bug_Tracker/Controllers/HomeController.cs
@@ -14,36 +14,38 @@ namespace Bug_Tracker.Controllers
         public ActionResult Index()
         {
             if ((System.Web.HttpContext.Current.User != null) && System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
-            {
-                if (User.IsInRole("admin"))
-                {
-                    var recentTickets = db.Tickets.OrderByDescending(t => t.Updated).Take(5).ToList();
-                    return RedirectToAction("Dashboard", recentTickets);
-                }
-                else if (User.IsInRole("manager"))
-                {
-                    var user = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
-                    var recentProjects = User.Identity;
-                    return RedirectToAction("Dashboard", recentProjects);
-                }
-                else if (User.IsInRole("developer"))
-                {
-                    return View();
-                }
-                else if (User.IsInRole("submitter"))
-                {
-                    return View();
-                }
-                else
-                    return View();
-            }
+                return RedirectToAction("Dashboard");
             else
                 return View();
         }
 
         public ActionResult Dashboard()
         {
-            return View();
+            if (User.IsInRole("admin"))
+            {
+                var recentTickets = db.Tickets.OrderByDescending(t => t.Updated).Take(5).ToList();
+
+                return View(recentTickets);
+            }
+            else if (User.IsInRole("manager"))
+            {
+                var user = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+                var recentProjects = user.ProjectUsers.OrderByDescending
+                    (p => p.Project.Tickets.OrderByDescending
+                    (t => t.Updated)).Take(5).ToList();
+
+                return View(recentProjects);
+            }
+            else if (User.IsInRole("developer"))
+            {
+                return View();
+            }
+            else if (User.IsInRole("submitter"))
+            {
+                return View();
+            }
+            else
+                return View();
         }
     }
 }

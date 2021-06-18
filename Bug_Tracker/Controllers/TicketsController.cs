52c67b2d96f8aa29fac0e37e9300722302e36f33
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2021-06-17 20:40:54 -0500

    download ticket attachments

--- Bug_Tracker/Controllers/TicketsController.cs
+++ Bug_Tracker/Controllers/TicketsController.cs
@@ -265,6 +265,16 @@ namespace Bug_Tracker.Controllers
             return RedirectToAction("Details", new { id = ticketId });
         }
 
+        //public ActionResult ViewAttachment(string filePath)
+        //{
+        //    
+        //}
+        public FileResult DownloadFile(string fileUrl, string filePath)
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(fileUrl);
+            return File(bytes, "application/octet-stream", filePath);
+        }
+
         [HttpPost]
         public ActionResult AssignDeveloper(int? id, string userId)
         {
